ae062a09cf8f75a26aa41078d0dafaf88acac730
ruan-barbosa <ruanrodriguesbarbosa08@gmail.com>
2026-05-26 22:20:53 -0300

    atualização pasta models

---
+++ gerenciamento_estoque/app/models/entradas_estoque.py
@@ -0,0 +1,6 @@
+class entradas_estoque:
+    def __init__(self, id_entrada, id_produto, quantidade, data_entrada):
+        self.id_entrada = id_entrada
+        self.id_produto = id_produto
+        self.quantidade = quantidade
+        self.data_entrada = data_entrada
---
+++ gerenciamento_estoque/app/models/saidas_estoque.py
@@ -0,0 +1,6 @@
+class entradas_estoque:
+    def __init__(self, id_saida, id_produto, quantidade, data_saida):
+        self.id_saida = id_saida
+        self.id_produto = id_produto
+        self.quantidade = quantidade
+        self.data_saida = data_saida
