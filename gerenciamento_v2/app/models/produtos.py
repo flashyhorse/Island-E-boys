66c288c6437251df04e2f506e1224bfb42c87991
ruan-barbosa <ruanrodriguesbarbosa08@gmail.com>
2026-06-06 22:25:51 -0300

    tirando os arquivos que ficaram sla pq

--- gerenciamento_v2/app/models/produtos.py
+++
@@ -1,7 +0,0 @@
-class Produto:
-    def __init__(self, nome, preco, quantidade, localizacao, id=None):
-        self.id = id
-        self.nome = nome
-        self.preco = preco
-        self.quantidade = quantidade
-        self.localizacao = localizacao
