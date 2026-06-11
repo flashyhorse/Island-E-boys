07983025cc853bd8a86852a3a247d58a434c942a
RUAN GUILHERME RODRIGUES BARBOSA <ruan_g_barbosa@estudante.sesisenai.org.br>
2026-06-11 18:15:59 -0300

    Senha

--- gerenciamento/config.py
+++ gerenciamento/config.py
@@ -3,4 +3,4 @@ class Config:
     USUARIO = 'root'
     SENHA = ''
     BANCO = 'gerenciamento_estoque'
-    SECRET_KEY = 'chave_secreta_aqui'
+    SECRET_KEY = '2021c2199d688836867fe3137913d0cd4c243b2f987ba2f6a949e8a654ec2b4c'
